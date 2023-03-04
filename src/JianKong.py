b9c5ffb4c5d6346702d3c1e7f5db9f0a5e14c5b5
Weitong Chen <chenweitong1991@163.com>
2023-03-04 12:31:07 +0800

    增加监控

--- src/JianKong.py
+++ src/JianKong.py
@@ -9,18 +9,33 @@ import time
 import winsound
 from easyquotation_client import EasyQuotationClient
 
+
 def alarm(freq, duration):
+    # freq: 报警声音频率.
+    # duration: 报警持续时间.
     freq = max(int(freq), 300)
     freq = min(int(freq), 800)
     winsound.Beep(int(freq), int(duration))
     time.sleep(duration / 1000)
 
+
+def add_alarm_record(name, diff_ratio, alarm_dict):
+    if abs(diff_ratio) < 0.05 and abs(diff_ratio) >= 0.03:
+        alarm_dict[name] = 0.05
+    elif abs(diff_ratio) < 0.08 and abs(diff_ratio) >= 0.05:
+        alarm_dict[name] = max(alarm_dict[name], 0.08)
+    elif abs(diff_ratio) < 0.1 and abs(diff_ratio) >= 0.08:
+        alarm_dict[name] = max(alarm_dict[name], 0.1)
+    elif abs(diff_ratio) >= 0.1:
+        alarm_dict[name] = max(alarm_dict[name], 0.2)
+
+
 def print_diff_ratio(name, now_price, diff_ratio):
-    # 当涨跌幅超过3%时，进行提示.
     _str = "%.2f%%" % (diff_ratio * 100)
     if diff_ratio > 0:
         _str = '+' + _str
     print(name, now_price, _str)
+
 
 if __name__ == '__main__':
     easyquotation_client = EasyQuotationClient()
@@ -36,20 +51,28 @@ if __name__ == '__main__':
         pre_day_map[name] = pre_day_close_price
 
     # 持续获取当前股价.
+    alarm_dict = dict()
     while True:
         print("===== %s =====" % time.ctime())
         for name in name_list:
             if name not in pre_day_map:
                 continue
-            
+
             pre_day_close_price = pre_day_map[name]
             now_price = easyquotation_client.read_now_price(name)
-            diff_ratio = (now_price - pre_day_close_price) / pre_day_close_price
+            diff_ratio = (now_price - pre_day_close_price) / \
+                pre_day_close_price
             print_diff_ratio(name, now_price, diff_ratio)
             if abs(diff_ratio) < 0.03:
+                # 变化幅度较小，不报警.
+                continue
+            elif name in alarm_dict and diff_ratio < alarm_dict[name]:
+                # 该区间已报警过，不报警.
                 continue
             else:
                 alarm(abs(diff_ratio) * 10000, 1000)
+                add_alarm_record(name, diff_ratio, alarm_dict)
 
+        print(alarm_dict)
         print("================ END ================")
-        time.sleep(5)
+        time.sleep(30)
